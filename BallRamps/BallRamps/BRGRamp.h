a041feb47b365c5836940e53a26d229274073d55
ameriles <jameriles@gmail.com>
2014-08-16 14:19:35 +0000

    Ramp implemented

---
+++ BallRamps/BallRamps/BRGRamp.h
@@ -0,0 +1,13 @@
+//
+//  BRGRamp.h
+//  BallRamps
+//
+//  Created by Agustin on 16/08/14.
+//  Copyright (c) 2014 Agustin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BRGRamp : NSObject
+
+@end
---
+++ BallRamps/BallRamps/BRGRamp.m
@@ -0,0 +1,13 @@
+//
+//  BRGRamp.m
+//  BallRamps
+//
+//  Created by Agustin on 16/08/14.
+//  Copyright (c) 2014 Agustin. All rights reserved.
+//
+
+#import "BRGRamp.h"
+
+@implementation BRGRamp
+
+@end
